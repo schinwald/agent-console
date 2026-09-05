6580ffb3da89faa10e9b81ca91a09bd772e4f324
schinwald <jamesschinwald@gmail.com>
2026-09-05 10:49:23 -0400

    debug: log tmux pane command output

--- packages/backend/src/tmux.ts
+++ packages/backend/src/tmux.ts
@@ -87,8 +87,10 @@ export const isProcessInPane = (
 const listPanes = (): TmuxPane[] => {
   const args = ['list-panes', '-a', '-F', '#{pane_pid}\t#{session_name}\t#{window_id}\t#{pane_id}'];
   const result = runTmuxCommand(args);
+  const command = `tmux ${tmuxArgs(args).join(' ')}`;
+  logger.debug('tmux', 'pane discovery command result', { command, ...result });
   if (!result.success) {
-    logger.warn('tmux', 'pane discovery failed', { command: `tmux ${tmuxArgs(args).join(' ')}`, ...result });
+    logger.warn('tmux', 'pane discovery failed', { command, ...result });
     return [];
   }
   return result.stdout
